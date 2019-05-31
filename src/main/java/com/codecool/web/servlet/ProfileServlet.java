a0802d6bfa35b1250666bd255f9db6050ba9d220
Taraszovics Péter <gtarapeti@gmail.com>
2019-05-31 15:53:43 +0200

    seeing my coupons on profie

--- src/main/java/com/codecool/web/servlet/ProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileServlet.java
@@ -1,6 +1,14 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.CouponDao;
+import com.codecool.web.dao.ShopDao;
+import com.codecool.web.dao.database.DatabaseCouponDao;
+import com.codecool.web.dao.database.DatabaseShopDao;
+import com.codecool.web.model.Coupon;
 import com.codecool.web.model.User;
+import com.codecool.web.service.CouponService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleCouponService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,14 +16,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/protected/profile")
-public final class ProfileServlet extends HttpServlet {
+public final class ProfileServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = (User) req.getSession().getAttribute("user");
-        req.setAttribute("user", user);
-        req.getRequestDispatcher("profile.jsp").forward(req, resp);
+
+        try (Connection connection = getConnection(req.getServletContext())) {
+            CouponDao couponDao = new DatabaseCouponDao(connection);
+            ShopDao shopDao = new DatabaseShopDao(connection);
+            CouponService couponService = new SimpleCouponService(couponDao, shopDao);
+
+            List<Coupon> coupons = couponService.getByUserId(String.valueOf(user.getId()));
+
+            req.setAttribute("coupons", coupons);
+            req.setAttribute("user", user);
+            req.getRequestDispatcher("profile.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
 }
